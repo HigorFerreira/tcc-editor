eb8ac16f88710324c8c737eb2e1e99747253b0a9
Higor Ferreira <hfashigor@hotmail.com>
2023-11-27 20:18:24 -0300

    Gerando PDF pelo latex

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,5 +1,44 @@
+import latex from 'node-latex'
+import fs from 'fs'
+import path from 'path'
 
+async function main() {
+
+    console.log("====== Inside main function".padEnd(50, "="));
+
+    const file_path = path.resolve('latex.tex');
+    const output_path = path.resolve('output.pdf');
+    const input = fs.createReadStream(file_path);
+    const output = fs.createWriteStream(output_path);
+    console.log({ latex })
+    const pdf = latex(input);
+
+    pdf.pipe(output);
+    await (() => new Promise((resolve, reject) => {
+        pdf.on('error', err => {
+            // console.error("======= PDF ERROR", err);
+            reject(err);
+        });
+        pdf.on('finish', () => {
+            console.log("PDF Generated");
+            resolve(true);
+        })
+    }))();
+
+
+
+    console.log(path.resolve());
+    console.log("====== End main".padEnd(50, "="));
+    return JSON.stringify({
+        file_path,
+        output_path,
+    }, null, 4).replace(/\n/g, '<br>').replace(/\s/g, '&nbsp;');
+}
 
 export default async function Test(){
-    return <div>OK</div>
+    const result = await main();
+
+    return <div>
+        <div dangerouslySetInnerHTML={{ __html: result }}></div>
+    </div>
 }
