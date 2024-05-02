961717ed3c521943c1fab49d91cef7c4f6a5e96d
Higor Ferreira <hfashigor@hotmail.com>
2024-05-02 11:06:36 -0300

    Starting parser

---
+++ src/parser/escape.ts
@@ -0,0 +1,15 @@
+export function escapeCharacters(str: string){
+    let newStr = str;
+    newStr = newStr.replace(/\\/gm, '\\textbackslash ');
+    newStr = newStr.replace(/#/gm, '\\#');
+    newStr = newStr.replace(/\$/gm, '\\$$');
+    newStr = newStr.replace(/%/gm, '\\%');
+    newStr = newStr.replace(/\^/gm, '\\textasciicircum ');
+    newStr = newStr.replace(/&/gm, '\\&');
+    newStr = newStr.replace(/_/gm, '\\_');
+    newStr = newStr.replace(/\{/gm, '\\{');
+    newStr = newStr.replace(/\}/gm, '\\}');
+    newStr = newStr.replace(/~/gm, '\\textasciitilde ');
+
+    return newStr;
+}
