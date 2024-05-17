c4a78721f12f18141aee78f448997b4bd31f27e9
Higor Ferreira <hfashigor@hotmail.com>
2024-05-17 17:45:06 -0300

    🚧 wip: Spliting refs and gloss

---
+++ src/tests/gloss.ts
@@ -0,0 +1,121 @@
+import {
+    HeaderBlock,
+    ImageBlock,
+    ParagraphBlock,
+    RefsObjectType,
+    GlossaryObjectType,
+    PageBreakBlock,
+} from '@/parser/types'
+
+export const gloss = {
+    'abnt': {
+        short: 'ABNT',
+        label: 'Associação Brasileira de Normas Técnicas',
+        type: 'sigla'
+    },
+    'mee': {
+        short: 'M.E.E',
+        label: 'Mestre em Engenharia Elétrica',
+        type: 'sigla',
+    },
+    'nbr': {
+        short: 'NBR',
+        label: 'Norma Brasileira Regulamentadora',
+        type: 'sigla'
+    },
+    'ies': {
+        label: 'Instituição de Ensino Superior',
+        short: 'IES',
+        type: 'sigla',
+    },
+    'pucgo': {
+        short: 'PUC-GO',
+        label: 'Pontifícia Universidade Católica de Goiás',
+        type: 'sigla',
+    },
+    'tcc': {
+        short: 'TCC',
+        label: 'Trabalho de Conclusão de Curso',
+        type: 'sigla',
+    },
+    'ufpb': {
+        short: 'UFPB',
+        label: 'Universidade Federal da Paraíba',
+        type: 'sigla',
+    },
+    'html': {
+        short: 'HTML',
+        label: 'HyperText Markup Language',
+        type: 'sigla',
+    },
+    'css': {
+        short: 'CSS',
+        label: 'Cascading Style Sheet',
+        type: 'sigla',
+    },
+    'http': {
+        short: 'HTTP',
+        label: 'Hypertext Transfer Protocol',
+        type: 'sigla',
+    },
+    'cern': {
+        short: 'CERN',
+        label: 'Conseil Européen pour la Recherche Nucléaire',
+        type: 'sigla',
+    },
+    'cms': {
+        short: 'CMS',
+        label: 'Content Management System',
+        type: 'sigla',
+    },
+    'xml': {
+        short: 'XML',
+        label: 'eXtensible Markup Language',
+        type: 'sigla',
+    },
+    'xhtml': {
+        short: 'XHTML',
+        label: 'eXtensible HyperText Markup Language',
+        type: 'sigla',
+    },
+    'svg': {
+        short: 'SVG',
+        label: 'Scalable Vector Graphics',
+        type: 'sigla',
+    },
+    'ecma': {
+        short: 'ECMA',
+        label: 'European Computer Manufacturers Association',
+        type: 'sigla',
+    },
+    'Prof': {
+        short: 'Prof',
+        label: 'Professor',
+        type: 'abreviacao',
+    },
+    'W3C': {
+        short: 'W3C',
+        label: 'World Wide Web Consortium',
+        type: 'abreviacao',
+    },
+    'MathML': {
+        short: 'MathML',
+        label: 'Mathematical Markup Language',
+        type: 'abreviacao',
+    },
+    'JS': {
+        short: 'JS',
+        label: 'JavaScript',
+        type: 'abreviacao',
+    },
+    'PHP': {
+        short: 'PHP',
+        label: 'Hypertext Preprocessor',
+        type: 'abreviacao',
+    },
+    'XSS': {
+        short: 'XSS',
+        label: 'Cross-Site Scripting',
+        type: 'abreviacao',
+    },
+} satisfies GlossaryObjectType
---
+++ src/tests/refs.ts
@@ -0,0 +1,55 @@
+import {
+    RefsObjectType,
+} from '@/parser/types'
+
+export const refs = {
+    'severino': {
+        type: 'book',
+        title: 'Metodologia do trabalho científico',
+        author: [ 'Antônio Joaquim Severino' ],
+        publisher: 'Cortez',
+        edition: 24,
+        year: 2017
+    },
+    'castro': {
+        type: 'book',
+        author: [ 'Claudio Moura Castro' ],
+        title: 'Como redigir e apresentar um trabalho científico',
+        publisher: 'São Paulo: Pearson Prentice Hall',
+        year: 2011
+    },
+    'abnt': {
+        type: 'misc',
+        author: [ 'ABNT' ],
+        title: 'Quem somos? ABNT - Associação Brasileira de Normas Técnicas',
+        url: 'http://www.abnt.org.br/institucional/sobre',
+        year: 2021,
+        note: '[Online; acessado em 15-Maio-2023]'
+    },
+    'medeiros': {
+        type: 'book',
+        author: [ 'João Bosco Medeiros',  ],
+        title: 'Redação científica: a prática de fichamentos, resumos, resenhas',
+        edition: 11,
+        publisher: 'São Paulo: Atlas',
+        year: 2012
+    },
+    'SilvaVitoria': {
+        type: 'article',
+        author: [ 'Marcelo Oliveira da Silva', 'Maria Inês Corte Vitoria' ],
+        title: 'A experiência de escrita no \
+        \rtrabalho de conclusão de curso: Percepções de alunos de um curso superior de \
+        \rtecnologia em gestão em recursos humanos',
+        publisher: 'CAMINE: Caminhos da Educação, Franca',
+        edition: 6,
+        year: 2014,
+        issn: '2175-4217'
+    },
+    'santos': {
+        type: 'article',
+        author: [ 'Ivna Regina Santos' ],
+        title: 'As dificuldades na construção do trabalho de conclusão de curso: Percepção de estudantes egressos do curso de ciências contábeis. 2020. 69. Trabalho de Conclusão de Curso (Graduação) - Ciências Contábeis',
+        publisher: 'Universidade Federal da Paraíba, João Pessoa',
+        year: 2020
+    }
+} satisfies RefsObjectType
